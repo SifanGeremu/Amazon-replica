4b864b6ecc205d8edc522c37c0c7c01e2f4822fd
SifanGeremu <wins33975@gmail.com>
2025-04-11 22:17:40 -0500

    final commit

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-f783d/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-f783d/us-central1/api",
+  baseURL: "https://amazon-api-qodt.onrender.com/clear",
 });
 export {axiosInstance};
